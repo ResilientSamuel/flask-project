7539b38ebe3e82b7d7d60d58c2f44db68865aab8
Samuel Owusu Agyemang <resilientcounsellor@gmail.com>
2017-07-19 15:03:06 +0000

    initial commit

--- myapp.py
+++ myapp.py
@@ -1,11 +1,30 @@
 
 from flask import Flask, render_template
+import os
+import paho.mqtt.client as mqtt
+
+client = mqtt.client()
+client.connect("moorhouseassociates.com", 1883, 60)
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template("index.html")
+	return render_template("index.html", to="Sam Moorhouse")
+
+@app.route('/btn')
+def btn():
+	print("button clicked")
+	client.publish("test/all", "this is my message to you"
+	return ""
+
+@app.route('/css/<path:path')
+def send_css(path):
+	return send_from_directory('css', path)
+
+@app.route('/img/<path:path>)
+def send_img(path):
+	return send_from_directory('img', path)
 
 @app.route('/python')
 def python():
